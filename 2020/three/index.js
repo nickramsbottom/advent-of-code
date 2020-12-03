dd64630cea99bc55aa737e0e718888c67508532d
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-03 08:58:01 +0000

    Add solution to 2020 day 3 part 2

--- 2020/three/index.js
+++ 2020/three/index.js
@@ -1,29 +1,42 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf8').split('\n').map(row => row.split(''));
 
-let x = 0;
-let y = 0;
+function countTrees(right, down) {
+	let x = 0;
+	let y = 0;
 
-const xInc = 3;
-const yInc = 1;
+	const xInc = right;
+	const yInc = down;
 
-const yLength = input.length;
-const xLength = input[0].length;
+	const yLength = input.length;
+	const xLength = input[0].length;
 
-const treeChar = '#';
+	const treeChar = '#';
 
-let count = 0;
-let call = 0;
-while(y < yLength) {
-	call += 1;
-	const char = input[y][x];
+	let count = 0;
+	while(y < yLength) {
+		const char = input[y][x];
 
-	if (char === treeChar) {
-		count += 1;
+		if (char === treeChar) {
+			count += 1;
+		}
+
+		y += yInc;
+		x = (x + right) % xLength;
 	}
 
-	y += yInc;
-	x = (x + 3) % xLength;
+	return count;
 }
 
-console.log(count);
+console.log(countTrees(3, 1));
+
+const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+
+const allSlopes = slopes.reduce(
+	(product, [right, down]) => {
+		return product * countTrees(right, down);
+	},
+	1
+);
+
+console.log(allSlopes);
