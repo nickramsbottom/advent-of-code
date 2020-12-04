e9eba3589d266a6551c4ad27336d5ec80c7dacfa
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-04 06:47:46 +0000

    Add solution to 2020 day 4 part 2

--- 2020/four/index.js
+++ 2020/four/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const records = fs.readFileSync('./input.txt', 'utf8').split(/\n\n/);
+const numberOfRecords = records.length;
+console.log('numberOfRecords', numberOfRecords);
 
 function parseRecord(record) {
 	const parts = record.split(/[\n ]+/);
@@ -11,6 +13,56 @@ function parseRecord(record) {
 		},
 		{}
 	);
+}
+
+function validateYear(yearEntry, from, to) {
+	const regex = /^\d{4}$/;
+	const validFormat = regex.test(yearEntry);
+
+	if (!validFormat) {
+		return false;
+	}
+
+	const year = Number(yearEntry);
+
+	return yearEntry >= from && yearEntry <= to;
+}
+
+function validateHeightEntry(heightEntry) {
+	const regex = /^(\d+)(cm|in)$/;
+
+	const matches = regex.exec(heightEntry);
+
+	if (matches === null) {
+		return false;
+	}
+
+	const value = Number(matches[1]);
+	const units = matches[2];
+
+	if (units === 'cm') {
+		return value >= 150 && value <= 193;
+	}
+
+	if (units === 'in') {
+		return value >= 59 && value <= 76;
+	}
+
+	return false;
+}
+
+function validateHairColor(colorEntry) {
+	const regex = /^#[\da-z]{6}$/;
+	return regex.test(colorEntry);
+}
+
+function validateEyeColor(colorEntry) {
+	return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].indexOf(colorEntry) !== -1;
+}
+
+function validatePassportId(idEntry) {
+	const regex = /^\d{9}$/;
+	return regex.test(idEntry);
 }
 
 let validRecords = 0;
@@ -26,7 +78,15 @@ for(const record of records) {
 		hgt
 	} = parseRecord(record);
 
-	if (iyr && pid && eyr && hcl && ecl && byr && hgt) {
+	const passId = validatePassportId(pid);
+	const eyeCol = validateEyeColor(ecl);
+	const hairCol = validateHairColor(hcl);
+	const height = validateHeightEntry(hgt);
+	const birth = validateYear(byr, 1920, 2002);
+	const issue = validateYear(iyr, 2010, 2020);
+	const expire = validateYear(eyr, 2020, 2030);
+
+	if (passId && eyeCol && hairCol && height && birth && issue && expire) {
 		validRecords++
 	}
 }
