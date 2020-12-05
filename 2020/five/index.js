a663cd83a74fce6bcdc1b3f6300e437d729efdd9
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-05 09:14:12 +0000

    Add solution to 2020 day 5 part 2

--- 2020/five/index.js
+++ 2020/five/index.js
@@ -43,4 +43,16 @@ const highestSeatId = input.reduce(
 	0
 );
 
-console.log(highestSeatId);
+const seatIds = input.map(calculateSeatId).sort((a, b) => a - b);
+
+const lowestId = seatIds[1];
+const highestId = seatIds[seatIds.length - 1];
+
+let predictedId = lowestId;
+for (let i = 1; i < seatIds.length; i++) {
+	if (predictedId !== seatIds[i]) {
+		console.log(predictedId);
+		break;
+	}
+	predictedId++;
+}
