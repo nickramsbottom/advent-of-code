c8fd6d192d87e42d4c934c30786ef5f4dfcdea6a
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-05 09:02:02 +0000

    Add solution to 2020 day 5 part 1

---
+++ 2020/five/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf8').split('\n');
+
+const exampleCode = ['F', 'B', 'F', 'B', 'B', 'F', 'F', 'R', 'L', 'R'];
+
+function calculateRow(code) {
+	let higherIndex = Math.pow(2, code.length) - 1;
+	let lowerIndex = 0;
+
+	for (const char of code) {
+		const length = higherIndex - lowerIndex;
+		const distance = (length - 1) / 2;
+
+		if (char === 'F' || char === 'L') {
+			higherIndex -= (distance + 1);
+		}
+
+		if (char === 'B' || char === 'R') {
+			lowerIndex += (distance + 1);
+		}
+	}
+
+	return lowerIndex;
+}
+
+function calculateSeatId(barcode) {
+	const rowPortion = barcode.slice(0, 7);
+	const seatPortion = barcode.slice(7);
+
+	return (8 * calculateRow(rowPortion)) + calculateRow(seatPortion);
+}
+
+const highestSeatId = input.reduce(
+	(highestId, code) => {
+		const newSeatId = calculateSeatId(code);
+
+		if (newSeatId > highestId) {
+			highestId = newSeatId;
+		}
+
+		return highestId;
+	},
+	0
+);
+
+console.log(highestSeatId);
