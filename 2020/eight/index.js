7b5ffb0fc6910a48bffd4deb0ee8117c7f116b2a
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-08 09:23:17 +0000

    Add solution to 2020 day 8 part 2

--- 2020/eight/index.js
+++ 2020/eight/index.js
@@ -11,31 +11,68 @@ const input = fs
 		};
 	});
 
-let accumulator = 0;
-let index = 0;
-while(true) {
-	const instruction = input[index];
+function computeAccumulator(instructions) {
+	let accumulator = 0;
+	let index = 0;
 
-	if(instruction.timesExecuted > 0) {
-		console.log(accumulator);
+	while(true) {
+		const instruction = instructions[index];
+
+		if (index === input.length) {
+			return accumulator;
+		}
+
+		if (instruction.timesExecuted > 0) {
+			throw Error(`in a loop, accumulator was: ${accumulator}`);
+		}
+
+		instruction.timesExecuted++;
+		const command = instruction.command;
+		if (command === 'acc') {
+			accumulator += instruction.value;
+			index++;
+			continue;
+		}
+
+		if (command === 'jmp') {
+			index += instruction.value;
+			continue;
+		}
+
+		if (command === 'nop') {
+			index++;
+			continue;
+		}
+	}
+}
+
+const toTry = [];
+for (let i = 0; i < input.length; i++) {
+	const instruction = input[i];
+	if (instruction.command === 'acc') {
+		continue;
+	}
+
+	const inputCopy = JSON.parse(JSON.stringify(input));
+
+	if (instruction.command === 'jmp') {
+		inputCopy[i].command = 'nop';
+	}
+
+	if (instruction.command === 'nop') {
+		inputCopy[i].command = 'jmp';
+	}
+
+	toTry.push(inputCopy);
+}
+
+for (let i = 0; i < toTry.length; i++) {
+	try {
+		console.log(computeAccumulator(toTry[i]));
 		break;
-	}
-
-	instruction.timesExecuted++;
-	const command = instruction.command;
-	if (command === 'acc') {
-		accumulator += instruction.value;
-		index++;
-		continue;
-	}
-
-	if (command === 'jmp') {
-		index += instruction.value;
-		continue;
-	}
-
-	if (command === 'nop') {
-		index++;
+	} catch (e) {
 		continue;
 	}
 }
+
+// console.log(computeAccumulator(input));
