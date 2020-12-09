25ece641361ce89cc8b3a7dea7a18cf80ed5f1ba
Nick Ramsbottom <hi@nickramsbottom.com>
2020-12-09 07:53:33 +0000

    Add solution to 2020 day 9 part 2

--- 2020/nine/index.js
+++ 2020/nine/index.js
@@ -5,7 +5,7 @@ const input = fs
 	.map(string => Number(string));
 
 const preambleLength = 25;
-// const invalidEntry = 14360655;
+const invalidEntry = 14360655;
 
 function findInvalidNumber() {
 	for(let i = preambleLength; i < input.length; i++) {
@@ -33,4 +33,60 @@ function complementInArray(target, numbers) {
 	return false;
 }
 
-console.log(findInvalidNumber());
+// console.log(findInvalidNumber());
+
+const numbers = [
+	714065,
+	715497,
+	776112,
+	802598,
+	749876,
+	841964,
+	776204,
+	835117,
+	823680,
+	844572,
+	851440,
+	830638,
+	846284,
+	906078,
+	1248266,
+	880860,
+	917404,
+];
+
+function findSlice() {
+	for (let i = 0; i < input.length; i++) {
+		const currentSlice = input.slice(i);
+
+		let sum = 0;
+		for (let j = 0; j < currentSlice.length; j++) {
+			const number = currentSlice[j];
+			sum += number;
+
+			if (sum === invalidEntry) {
+				return {
+					from: i,
+					to: i + j,
+				}
+			}
+
+			if (sum > invalidEntry) {
+				break;
+			}
+		}
+	}
+}
+
+function findSum() {
+	const { from, to } = findSlice();
+
+	const slice = input.slice(from, to);
+	slice.sort((a, b) => a - b);
+
+	const sum = slice[0] + slice[slice.length - 1];
+
+	console.log(sum);
+}
+
+findSum();
