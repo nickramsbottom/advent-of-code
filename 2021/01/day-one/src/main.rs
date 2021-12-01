edb584d6f6e6a028cd4a53505d43fdd62c8c7c49
Nick Ramsbottom <hi@nickramsbottom.com>
2021-12-01 09:54:30 +0000

    Day 1 Part 2 Rust

--- 2021/01/day-one/src/main.rs
+++ 2021/01/day-one/src/main.rs
@@ -11,11 +11,16 @@ fn main() {
         .map(|line| line.unwrap().parse::<usize>().unwrap())
         .collect();
 
+    let mut sum = ns[0] + ns[1] + ns[2];
     let mut c = 0;
-    for i in 1..ns.len() {
-        if ns[i] > ns[i - 1] {
+    for w in ns.windows(3).skip(1) {
+        let new_sum = w.iter().sum();
+
+        if new_sum > sum {
             c += 1;
         }
+
+        sum = new_sum;
     }
 
     println!("{}", c)
