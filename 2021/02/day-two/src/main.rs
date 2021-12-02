12ab19c3f4e3c7ef8bd0d16af618b199c23ed779
Nick Ramsbottom <hi@nickramsbottom.com>
2021-12-02 09:03:01 +0000

    Day 2 Part 1 Rust

--- 2021/02/day-two/src/main.rs
+++ 2021/02/day-two/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("./input.txt").unwrap();
+    let r = BufReader::new(f);
+
+    let ns: Vec<String> = r.lines().map(|line| line.unwrap()).collect();
+
+    let mut depth = 0;
+    let mut hor = 0;
+
+    for n in ns {
+        let split = n.split(' ').collect::<Vec<&str>>();
+        let dir = split[0];
+        let amt = split[1].parse::<i32>().unwrap();
+
+        match dir {
+            "forward" => hor += amt,
+            "down" => depth += amt,
+            "up" => depth -= amt,
+            _ => println!("Invalid input"),
+        }
+    }
+
+    println!("{:?}", depth * hor)
 }
