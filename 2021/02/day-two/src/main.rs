dfcbaba1317bf01045aa99d6ab9213710995dd96
Nick Ramsbottom <hi@nickramsbottom.com>
2021-12-02 09:36:48 +0000

    Refactor to enum for direction

--- 2021/02/day-two/src/main.rs
+++ 2021/02/day-two/src/main.rs
@@ -2,29 +2,48 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
+enum Dir {
+    Forward,
+    Down,
+    Up,
+}
+
 fn main() {
     let f = File::open("./input.txt").unwrap();
     let r = BufReader::new(f);
 
-    let ns: Vec<String> = r.lines().map(|line| line.unwrap()).collect();
+    let ns: Vec<(Dir, i32)> = r
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let parts = line.split(' ').collect::<Vec<&str>>();
+            let amt = parts[1].parse::<i32>().unwrap();
+
+            let dir = match parts[0] {
+                "forward" => Dir::Forward,
+                "down" => Dir::Down,
+                "up" => Dir::Up,
+                _ => panic!("Invalid direction"),
+            };
+
+            (dir, amt)
+        })
+        .collect();
 
     let mut depth = 0;
     let mut hor = 0;
     let mut aim = 0;
 
     for n in ns {
-        let split = n.split(' ').collect::<Vec<&str>>();
-        let dir = split[0];
-        let amt = split[1].parse::<i32>().unwrap();
+        let (dir, amt) = n;
 
         match dir {
-            "forward" => {
+            Dir::Forward => {
                 hor += amt;
                 depth += aim * amt
             }
-            "down" => aim += amt,
-            "up" => aim -= amt,
-            _ => println!("Invalid input"),
+            Dir::Down => aim += amt,
+            Dir::Up => aim -= amt,
         }
     }
 
