860504742a316fdec92eb54ee218b5276d5a14a5
Nick Ramsbottom <hi@nickramsbottom.com>
2021-12-08 13:22:29 +0000

    Find 5 and 6

--- 2021/08/wires/src/main.rs
+++ 2021/08/wires/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn main() {
@@ -17,16 +18,50 @@ fn main() {
 }
 
 fn find_output(signal_patterns: Vec<(Vec<&str>, Vec<&str>)>) -> usize {
-    let mut count = 0;
+    let mut numbers = HashMap::<&str, u8>::new();
 
     for (pattern, _output) in signal_patterns {
-        for input in pattern {
+        let mut first_three_mapping = HashMap::<&str, &str>::new();
+
+        for input in pattern.iter() {
             match input.len() {
-                2 | 3 | 4 | 7 => count += 1,
-                _ => (),
+                2 => numbers.insert(input, 1),
+                3 => numbers.insert(input, 7),
+                4 => numbers.insert(input, 4),
+                7 => numbers.insert(input, 8),
+                _ => None,
+            };
+        }
+
+        for input in pattern.iter() {
+            let len = input.len();
+
+            if len < 3 {
+                continue;
+            }
+
+            let first_three = &input[0..3];
+            let complement = first_three_mapping.get(first_three);
+
+            match complement {
+                Some(complement) => {
+                    let len = input.len();
+                    if len == 5 {
+                        numbers.insert(input, 5);
+                        numbers.insert(complement, 6);
+                    } else {
+                        numbers.insert(input, 6);
+                        numbers.insert(complement, 5);
+                    }
+                }
+                None => {
+                    first_three_mapping.insert(first_three, input);
+                }
             }
         }
     }
 
-    count
+    println!("{:?}", numbers);
+
+    10
 }
