b50aa9c673b59561a144a8664643f72e0715f41b
Nick Ramsbottom <nick.ramsbottom@ft.com>
2021-12-10 09:37:08 +0000

    Day 9 part 2 Rust

--- 2021/09/smoke/src/main.rs
+++ 2021/09/smoke/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::read_to_string;
+use std::collections::HashSet;
 
 fn main() {
     let input = read_to_string("./input.txt").expect("failed to open input file");
@@ -8,7 +9,51 @@ fn main() {
         .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
 
-    println!("{}", calc_risk_level(heights));
+    let low_points = calc_low_points(&heights);
+
+    let mut sizes: Vec<u32> = low_points
+        .into_iter()
+        .map(|low_point| walk_valley(&low_point, &heights))
+        .collect::<Vec<_>>();
+
+    sizes.sort();
+    sizes.reverse();
+
+
+    println!("{:?}", sizes.iter().take(3).product::<u32>());
+}
+
+fn walk_valley(point: &(i32, i32), map: &Vec<Vec<u32>>) -> u32 {
+    let x = point.0;
+    let y = point.1;
+    let mut stack = vec![(x, y)];
+    let mut visited = HashSet::new();
+    let height = map.len() as i32;
+    let width = map[0].len() as i32;
+    let mut count = 0;
+
+    while let Some(walk_point) = stack.pop() {
+        if !visited.insert(walk_point) {
+            continue;
+        }
+
+        let value = map[walk_point.0 as usize][walk_point.1 as usize];
+
+        find_neighbours(walk_point, width, height)
+            .iter()
+            .filter(|n| {
+                let i = n.0 as usize;
+                let j = n.1 as usize;
+                (map[i][j] != 9) & (value < map[i][j])
+            })
+            .for_each(|(i, j)| {
+                stack.push((*i, *j))
+            });
+
+        count += 1
+    }
+
+    count
 }
 
 fn calc_risk_level(map: Vec<Vec<u32>>) -> u32 {
