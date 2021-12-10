eafda6cd46167df48274bba3a61dbba0665c3a9f
Nick Ramsbottom <nick.ramsbottom@ft.com>
2021-12-10 13:16:49 +0000

    Day 10 Part 2 JavaScript

--- 2021/10/index.js
+++ 2021/10/index.js
@@ -4,22 +4,56 @@ const input = fs.readFileSync('./input.txt', 'utf8')
 	
 const lines = input.split('\n').map(line => line.split(''));
 
+const complements = {
+    '}': '{',
+    '>': '<',
+    ']': '[',
+    ')': '(',
+}
+
 const score = lines.reduce(
     (score, line) => score += scoreLine(line),
     0
 );
 
-console.log(score);
+const incompleteLines = lines.filter(line => scoreLine(line) === 0);
+const scores = incompleteLines
+    .map(line => completeLineScore(line))
+    .sort((a, b) => a - b);
+
+console.log(scores[(scores.length + 1) / 2 - 1]);
+
+function completeLineScore(line) {
+    const stack = [];
+
+    for (char of line) {
+        if (!complements[char]) {
+            stack.push(char);
+        } else {
+            stack.pop();
+        }
+    }
+
+    const scoresTable = {
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4,
+    }
+
+    let score = 0;
+    let i = 0;
+    while (stack.length > 0) {
+        const char = stack.pop();
+        score = (score * 5) + scoresTable[char];
+        i++;
+    }
+
+    return score;
+}
 
 function scoreLine(line) {
     const stack = [];
-
-    const complements = {
-        '}': '{',
-        '>': '<',
-        ']': '[',
-        ')': '(',
-    }
 
     const scores = {
         ')': 3,
