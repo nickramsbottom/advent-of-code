962a0761a4f1ec7e97c96b6b08f2a6d778b4c4c1
Nick Ramsbottom <hi@nickramsbottom.com>
2021-12-13 16:33:35 +0000

    Day 12 Part 1 JavaScript
    Lifted from here: https://github.com/joeyemerson/aoc/blob/main/2021/12-passage-pathing/solution.js
    
    Time to revise graphs and their traversal!

--- 2021/12/index.js
+++ 2021/12/index.js
@@ -1,24 +1,60 @@
 const fs = require('fs');
+const { setEnvironmentData } = require('worker_threads');
 const input = fs
     .readFileSync('./input.txt', 'utf8')
     .split('\n')
     .map(line => line.split('-'));
 
+console.log(part1(createGraph(input)));
 
-const graph = {};
+function createGraph(relations) {
+    const graph = {};
 
-for (line of input) {
-    const [firstCave, secondCave] = line;
-    if (!graph[firstCave]) {
-        graph[firstCave] = [];
+    for (line of relations) {
+        const [firstCave, secondCave] = line;
+        if (!graph[firstCave]) {
+            graph[firstCave] = [];
+        }
+
+        if (!graph[secondCave]) {
+            graph[secondCave] = [];
+        }
+
+        graph[firstCave].push(secondCave);
+        graph[secondCave].push(firstCave);
     }
 
-    if (!graph[secondCave]) {
-        graph[secondCave] = [];
-    }
-
-    graph[firstCave].push(secondCave);
-    graph[secondCave].push(firstCave);
+    return graph;
 }
 
-console.log(graph);
+function part1(graph) {
+    const caves = createGraph(input);
+    
+    function depthFirstSearch(cave, visited) {
+        if (cave === 'end') {
+            return 1;
+        }
+
+        if (visited.has(cave) && isSmallCave(cave)) {
+            return 0;
+        }
+
+        let result = 0;
+
+        visited.add(cave);
+
+        for (const nextCave of caves[cave]) {
+            result += depthFirstSearch(nextCave, visited);
+        }
+
+        visited.delete(cave);
+
+        return result;
+    }
+
+    return depthFirstSearch('start', new Set());
+}
+
+function isSmallCave(cave) {
+    return /[a-z]/.test(cave);
+}
